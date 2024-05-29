7f9a436aa3078765ca44344d8e9c79443282c4fd
wpulidoe <wilmer-951@hotmail.com>
2024-05-29 07:27:46 -0500

    primer compromiso

---
+++ PythonDataset/dataset1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = apd.read_csv('C:/xampp/htdocs/Python/PythonDataset/dataset1.csv',encoding='latin-1',sep=';')
+
+print(df.head())
---
+++ PythonDataset/pruebaconexionMysql.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import mysql.connector
+
+
+archivo = 'prueba.xlsx'
+
+df =pd.read_excel(archivo)
+
+
+conexion = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='basededatos'
+)
+cursor = conexion.cursor()
+
+# Itera sobre cada fila del DataFrame y realiza la inserción en la base de datos
+
+
+for indice_fila, fila in df.iterrows():
+    try:
+        # Suponiendo que tu tabla en MySQL tiene las mismas columnas que el DataFrame
+        query = "INSERT INTO tbprueba (Nombre, Materia) VALUES (%s, %s)"
+        valores = (fila['Nombre'], fila['Materia'])
+        cursor.execute(query, valores)
+        print("Inserción exitosa para la fila:", indice_fila)
+    except mysql.connector.Error as error:
+        print("Error al insertar fila:", indice_fila, "-", error)
+
+
+
+
+# Confirma los cambios en la base de datos
+conexion.commit()
+
+# Cierra la conexión
+conexion.close()
