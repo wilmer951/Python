5953f99faa0fd5c0dfbf00360e05e1672e773e4b
wpulidoe <wilmer-951@hotmail.com>
2024-05-29 11:22:00 -0500

    filtrado df

--- PythonDataset/dataset1.py
+++ PythonDataset/dataset1.py
@@ -2,7 +2,25 @@ import pandas as pd
 
 df = pd.read_csv('C:/xampp/htdocs/Python/PythonDataset/dataset1.csv',encoding='latin-1',sep=';')
 
-print(df[df.Numer>300])
+
+# SE REALIZA FILTRO DEL CSV POR AÑO MAYOR A 20 Y NUMER MAYOR A 5
+filtrar = (df['Year']>2020) & (df['Numer']> 5)
+
+
+# EL FILTROSE GUARDA EN OTRO DATAFRAME DF2 Y SE RESETEA LOS INDICES PARA NUEVAS CONSULTAS DEL DF2
+df2=df[filtrar].head(5).reset_index(drop=True)
+
+
+
+#IMPRIMIR DF2
+print(df2)
+
+print('Otro resultado')
+print(df2.loc[0,'Year'])
+
+
+
+
 
 
 #este es un cambio de prueba
