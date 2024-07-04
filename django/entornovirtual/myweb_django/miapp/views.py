09266bb730334dc3ee83263a3d2d27b8dab0fc7e
wpulidoe <wilmer-951@hotmail.com>
2024-07-04 09:09:08 -0500

    actualizacion proyecto enterno virutal estructuración de urls

--- django/entornovirtual/myweb_django/miapp/views.py
+++ django/entornovirtual/myweb_django/miapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def services(request):
+
+    return render(request, 'miapp/services.html')
+
+
+def about(request):
+
+    return render(request, 'miapp/about.html')
+
+
+def blog(request):
+
+    return render(request, 'miapp/blog.html')
+
+
+def contactus(request):
+
+    return render(request, 'miapp/contactus.html')
--- django/entornovirtual/myweb_django/myweb_django/urls.py
+++ django/entornovirtual/myweb_django/myweb_django/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myweb_django.views import home
+from django.urls import path, include
+from myweb_django import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),]
+    path('', views.home),
+    path('', include('miapp.urls')),
+
+
+]
